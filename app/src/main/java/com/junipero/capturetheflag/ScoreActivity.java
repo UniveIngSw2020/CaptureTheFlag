4e8652cf45a8365bad29ab6ee6463e1d69e85496
Melania Gottardo <melania.gottardo@gmail.com>
2020-12-19 13:37:25 +0100

    Fine del gioco per vicinanza (stealer) o controllo del db e creazione ScoreActivity che riporta nella MainActivity

---
+++ app/src/main/java/com/junipero/capturetheflag/ScoreActivity.java
@@ -0,0 +1,46 @@
+package com.junipero.capturetheflag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.TextView;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        Intent i = this.getIntent();
+        String team = i.getStringExtra("team");
+        String score = i.getStringExtra("score");
+
+        StoredDataManager sdm = new StoredDataManager(ScoreActivity.this.getFilesDir());
+        TextView scoreText = findViewById(R.id.score);
+
+        if(score.equals("Tie")){
+            sdm.increaseTies();
+            scoreText.setText("Tie");
+        } else if(score.equals(team + " wins")){
+            sdm.increaseWins();
+            scoreText.setText("You won");
+        } else {
+            sdm.increaseLosts();
+            scoreText.setText("You lost");
+        }
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(ScoreActivity.this,
+                        MainActivity.class));
+            }
+        }, 3000);
+    }
+}
